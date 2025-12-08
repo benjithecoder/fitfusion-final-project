ec2e1e3479313c5cb14ff282e30059edf2119502
Benjamin Borri <benjaminborri1@gmail.com>
2025-12-08 20:38:23 +0100

    Save project snapshot

---
+++ database/schema.sql
@@ -0,0 +1,52 @@
+-- Drop tables if they already exist 
+DROP TABLE IF EXISTS workout_logs;
+
+DROP TABLE IF EXISTS exercises;
+
+DROP TABLE IF EXISTS users;
+
+--user table
+CREATE TABLE
+    users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+--exercises table (on row per exercise type)
+CREATE TABLE
+    exercises (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        slug TEXT NOT NULL UNIQUE, -- eg. 'back_squat' this is for the computer 
+        name TEXT NOT NULL, -- eg. 'Back Squat' this is for the human who reads it
+        muscle_group TEXT NOT NULL -- eg. 'legs' This tells you what category the exercise belongs to:legs, chest , back,...
+    );
+
+--workout logs table (one row per logged set group)
+CREATE TABLE
+    workout_logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Each new log entry gets its own unique ID. like log 1 will get id = 1 , log nr 2 will get id = 2 etc 
+        user_id INTEGER NOT NULL, -- who is the logged in user ? tom? anna ? carl ? 
+        exercise_id INTEGER NOT NULL, -- This tells you which exercise the log belongs to.so ig squat = 1 and if the user is on the squat page, the log will always use: exercise_id = 1
+        weight REAL NOT NULL, -- The type REAL means it can store decimal numbers too. like 80.2 kg
+        reps INTEGER NOT NULL,
+        sets INTEGER NOT NULL,
+        logged_at DATE DEFAULT (DATE ('now')),
+        FOREIGN KEY (user_id) REFERENCES user (id), -- ❌ You cannot insert a log for a user that doesn't exist
+        FOREIGN KEY (exercise_id) REFERENCES exercises (id) -- ❌ You cannot insert a log for an exercise that doesn't exist
+    );
+
+--seed exercises (so i don't have to insert them manually later)
+INSERT INTO
+    exercises (slug, name, muscle_group)
+VALUES
+    ('back_squat', 'Back Squat', 'Legs'),
+    ('bench_press', 'Bench Press', 'Chest'),
+    ('barbell_row', 'Barbell Row', 'Back'),
+    ('bicep_curl', 'Bicep Curl', 'Arms'),
+    ('shoulder_press', 'Shoulder Press', 'Shoulders'),
+    ('tricep_extension', 'Tricep Extension', 'Triceps');
+
+-- Before users can log: back squat / bench pres, ...the app must KNOW those exercises exist so we tell it here that those are the 6 exercises that exist
+-- so back squat gets the id = 1 , bench press gets id= 2 , barbell row gets id=3 etc...
--- server/app.py
+++ server/app.py
@@ -1,21 +1,135 @@
-from flask import Flask
+import sqlite3
 
-# create the main app object
+from flask import Flask, render_template, g, request
+
+
 app = Flask(
     __name__,
-    static_folder="")
+    static_folder="../static",
+    template_folder="../templates"
+)
+
+# tells flask , if we ever need to use the database , here is where the database files lives.
+app.config["DATABASE"] = "database/fitfusion.db"
 
 
-# say's "when someone visits / , run the home() function"
+def get_db_connection():
+    """OPEN a connection to a sqlite database. we attach it to 'g' so 
+    it can be reused within a single request"""
+
+    if "db" not in g:
+        conn = sqlite3.connect(app.config["DATABASE"])
+        conn.row_factory = sqlite3.Row  # so i can access columns by name
+        g.db = conn
+    return g.db
+
+
+# Run the decorated function after every request, no matter what happens
+@app.teardown_appcontext
+def close_db_connection(exception=None):
+    """Automatically close the DB connection at the end of each request"""
+
+    # Try to take "db" out of the backpack(g), If it exists → give me the connection object, If it doesn’t → give me None , We are cleaning the backpack.
+    db = g.pop("db", None)
+    # Did we actually have a database connection in this request? if db is none , do nothing.
+    if db is not None:
+        db.close()
+
+
 @app.route("/")
 def home():
-    """
-    Very first test route. for now it just return simple text so i can confirm the server works.
-    """
-    return "Fitfusion backend is running 🚀"
+    return render_template("index.html")
 
 
-# let's you start a local server with python app.py
+@app.route("/back-squat.html", methods=["GET", "POST"])
+def back_squat():
+
+    # 1) open Database(db) connection
+    db = get_db_connection()
+
+    # 2) look up the exercise row for ' back_squat '
+    exercise = db.execute(
+        # fetchone return a row like {"id":1} if found or None if no such exercise exist.
+        "SELECT id FROM exercises WHERE slug = ?", ("back_squat",)).fetchone()
+
+    if exercise is None:
+        # this means the exercise table doesn't have 'back_squat'
+        print("ERROR: 'back_squat' not found in exercises table!")
+        return "exercise not found", 404
+
+    exercise_id = exercise["id"]
+    # this is TEMPORARY!!
+    user_id = 1
+
+    # 3) handles form submission (POST)
+    if request.method == "POST":
+        # for this step we just read the form values.
+        weight = request.form.get("weight")
+        # set is a build in python function so to not confuse the 2 i use sets_ .
+        sets_ = request.form.get("sets")
+        reps = request.form.get("reps")
+
+        # insert the new log row into workout_logs with this user, this exercise, this weight , these reps, and these sets.
+        db.execute(
+            """INSERT INTO workout_logs (user_id, exercise_id, weight, sets, reps) VALUES(?,?,?,?,?)""",
+            (user_id, exercise_id, weight, sets_, reps)
+        )
+        # this makes the changes permanent , without this the insert would be temporary.
+        db.commit()
+
+        print("LOG SAVED:",
+              "user_id=", user_id,
+              "exercise_id=", exercise_id,
+              "weight=", weight,
+              "reps=", reps,
+              "sets=", sets_
+              )
+
+    # always load logs (for both GET and after POST)
+    logs = db.execute(
+        """
+        SELECT weight, sets, reps, logged_at FROM workout_logs
+        WHERE user_id = ? AND exercise_id = ?
+        ORDER BY logged_at DESC
+        """,
+        (user_id, exercise_id)
+    ).fetchall()
+
+    return render_template("back-squat.html", logs=logs)
+
+
+@app.route("/chest.html")
+def chest():
+    return render_template("chest.html")
+
+
+@app.route("/back.html")
+def back():
+    return render_template("back.html")
+
+
+@app.route("/arms.html")
+def arms():
+    return render_template("arms.html")
+
+
+@app.route("/shoulders.html")
+def shoulders():
+    return render_template("shoulders.html")
+
+
+@app.route("/triceps.html")
+def triceps():
+    return render_template("triceps.html")
+
+
+@app.route("/debug/exercises")
+def debug_exercises():
+    db = get_db_connection()
+    rows = db.execute(
+        "SELECT slug, name, muscle_group FROM exercises").fetchall()
+    return "<br>".join(f"{row['slug']} - {row['name']} ({row['muscle_group']})" for row in rows)
+
+
 if __name__ == "__main__":
-    # debug=True auto-reloads the server when you change python files.
     app.run(debug=True)
